745584f2eff9ff8044ed20a037dcc4cee2e019cb
Brayan Gaytan <gaytangf17@gmail.com>
2021-09-18 17:37:13 -0500

    Practica 7

---
+++ ionic-basic/src/app/login/login.page.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/user.class';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { ModalErrorComponent } from '../modal-error/modal-error.component';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  user: User = new User();
+  constructor(private autSvc: AuthService, private router: Router,
+    private modalCtrl: ModalController) { }
+
+  ngOnInit() {
+  }
+
+  async onLogin(){
+    const user = await this.autSvc.onLogin(this.user);
+    if(user!=null && user.code ==undefined){
+      console.log('Successfully logged in!');
+      this.router.navigate(['/home']);
+    }
+    else{
+      if(user.code){
+        if(user.code=='auth/wrong-password' || user.code=='auth/invalid-email' || user.code=='auth/argument-erros'){
+          this.openModal(user);
+        }
+      }
+    }
+  }
+
+  async openModal(user:any){
+    const modal = await this.modalCtrl.create({
+      component: ModalErrorComponent,
+      componentProps:{
+        error: 'Ingres password y/o contraseña'
+      }
+    });
+    return await modal.present();
+  }
+  
+}
