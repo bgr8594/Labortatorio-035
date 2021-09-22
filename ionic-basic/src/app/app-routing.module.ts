5ca54f132cbb9e84bac99366125b0904a5b77d0c
David-VR0 <david.97.vargas@outlook.com>
2021-09-22 10:19:27 -0500

    practica 7 1era parte

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: 'home', canActivate: [AuthGuard],
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'alumnos',
+    path: '', 
+    redirectTo: 'recetas',
     pathMatch: 'full'
   },
   {
-    path: 'alumnos',
+    path: 'alumnos', canActivate: [AuthGuard],
     children: [
       {
         path:'',
@@ -25,17 +26,30 @@ const routes: Routes = [
     ]
   },
   {
-    path: 'recetas',
+    path: 'recetas', canActivate: [AuthGuard],
     loadChildren: () => import('./recetas/recetas.module').then( m => m.RecetasPageModule)
   },
   {
-    path: 'detalle-receta',
+    path: 'detalle-receta', canActivate: [AuthGuard],
     loadChildren: () => import('./detalle-receta/detalle-receta.module').then( m => m.DetalleRecetaPageModule)
   },
   {
-    path: 'tabs',
+    path: 'tabs', canActivate: [AuthGuard],
     loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'admin', canActivate: [AuthGuard],
+    loadChildren: () => import('./admin/admin.module').then( m => m.AdminPageModule)
+  },
+
   
 
 ];
--- ionic-basic/src/environments/environment.ts
+++ ionic-basic/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   firebaseConfig : {
+    apiKey: "AIzaSyDTr2Me6TwYa0DzbFuc_xOAAdw5Qf7xaYs",
+    authDomain: "ionic-basic-dbaa3.firebaseapp.com",
+    projectId: "ionic-basic-dbaa3",
+    storageBucket: "ionic-basic-dbaa3.appspot.com",
+    messagingSenderId: "1053377374580",
+    appId: "1:1053377374580:web:5c8c1f6a6fa51d53576421"
+  }
 };
 
 /*
