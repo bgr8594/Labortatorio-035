b81b3ca2398eba0d1addda7936211b9c4e6d18c8
America Gonzaga <america.gonzagatn@uanl.edu.mx>
2021-09-30 12:51:38 -0500

    Configurar base de datos firestore, alta y consulta de lugares, agregar pagina destinos y la ruta en el Guard CanActivate, agregarlos a menu

--- ionic-basic/src/app/app.module.ts
+++ ionic-basic/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { ComponentesModule } from './componentes/componentes.module';
 import { environment } from 'src/environments/environment'; 
 
@@ -17,7 +18,7 @@ import { environment } from 'src/environments/environment';
   entryComponents: [],
   imports: [ComponentesModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireAuthModule],
+    AngularFireAuthModule, AngularFirestoreModule ],
   providers: [NavParams, HttpClientModule, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
