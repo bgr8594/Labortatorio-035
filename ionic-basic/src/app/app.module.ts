167a76648536810d2750cc6c1f138464ac881f66
Brayan Gaytan <gaytangf17@gmail.com>
2021-09-30 22:37:12 -0500

    BD Firestore, Alta y Consulta de Lugares, Pagina de Destinos y ruta GuarCanActivate, agregarlos a Menu

--- ionic-basic/src/app/app.module.ts
+++ ionic-basic/src/app/app.module.ts
@@ -11,13 +11,14 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { ComponentesModule } from './componentes/componentes.module';
 import { environment } from 'src/environments/environment';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [ComponentesModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, 
   AngularFireModule.initializeApp(environment.firebaseConfig),
-  AngularFireAuthModule ],
+  AngularFireAuthModule, AngularFirestoreModule ],
   providers: [NavParams, HttpClientModule, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
