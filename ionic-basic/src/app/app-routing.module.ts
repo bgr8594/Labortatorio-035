443474be13474113367adbeca35eb5559bf07787
Marco Bracamontes <marco.bracamontesmz@uanl.edu.mx>
2021-10-06 15:30:35 +0200

    configurar base de datos firestore, altas y consulta de lugares, agregar pagina destinas y la ruta en el Guard csnActivate, Agregarlos a menu

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -49,6 +49,11 @@ const routes: Routes = [
     path: 'admin', canActivate: [AuthGuard],
     loadChildren: () => import('./admin/admin.module').then( m => m.AdminPageModule)
   },
+  {
+    path: 'destinos', canActivate: [AuthGuard],
+    loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule)
+  },
+
 
 
 
