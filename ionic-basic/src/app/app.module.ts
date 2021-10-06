b2af4fa221f3889fb9c210d54aac2d7fa60fcb0c
albertotienda <66221264+albertotienda@users.noreply.github.com>
2021-10-06 14:25:57 -0500

    p9: configurar base de datos firestore, alta y consulta de lugares, agregar pagina destinos y la ruta en el guard CanActivate, agregarlos a menu

--- ionic-basic/src/app/app.module.ts
+++ ionic-basic/src/app/app.module.ts
@@ -10,13 +10,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentesModule } from './componentes/componentes.module';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth'
+import { AngularFirestoreModule } from 'angularfire2/firestore'
 import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [ComponentesModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig), AngularFireAuthModule],
+    AngularFireModule.initializeApp(environment.firebaseConfig), AngularFireAuthModule, AngularFirestoreModule],
   providers: [NavParams, HttpClientModule, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ ionic-basic/src/app/destinos/destinos.page.ts
@@ -0,0 +1,27 @@
+import { ThrowStmt } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { LugaresService } from '../service/lugares.service';
+import { Lugar } from '../shared/lugar';
+
+@Component({
+  selector: 'app-destinos',
+  templateUrl: './destinos.page.html',
+  styleUrls: ['./destinos.page.scss'],
+})
+export class DestinosPage implements OnInit {
+
+  lugar: Lugar = new Lugar();
+  destinos: any[] = [];
+  constructor(private lugarService: LugaresService) { }
+
+  ngOnInit() {
+    this.lugarService.getLugares(this.destinos);
+  }
+
+  altaLugar(){
+    this.lugarService.altaLugar(this.lugar);
+    this.lugarService.getLugares(this.destinos);
+    this.lugar.nombre='';
+  }
+
+}
