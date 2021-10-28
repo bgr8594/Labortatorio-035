7ebf979b9d465fd328bf530a2237e64b3d9b44b0
America Gonzaga <america.gonzagatn@uanl.edu.mx>
2021-10-28 11:34:30 -0500

    npx cap init

---
+++ ionic-basic/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ionics.starter',
+  appName: 'ionic-basic-1917885',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
