32502bba35f76bfdfc3bad0cc5dbae2467aa11f5
Melanie González <melanie.gonzalezgar@uanl.edu.mx>
2021-10-27 22:57:13 -0500

    Practica 13, Acceso a camara, galeria, capacitor, preparacion para apk android

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -53,6 +53,11 @@ const routes: Routes = [
     path: 'destinos', canActivate: [AuthGuard],
     loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule)
   },
+  {
+    path: 'galeria', canActivate: [AuthGuard],
+    loadChildren: () => import('./galeria/galeria.module').then( m => m.GaleriaPageModule)
+  },
+
 
 
   
