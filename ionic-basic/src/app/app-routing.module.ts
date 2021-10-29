67f85d1a2ed8403906a54fd77f640e4f54b69244
albertotienda <66221264+albertotienda@users.noreply.github.com>
2021-10-29 15:02:50 -0500

    practica13: acceso a camara, capacitor, preparacion para apk android

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -51,7 +51,11 @@ const routes: Routes = [
   {
     path: 'destinos', canActivate: [AuthGuard],
     loadChildren: () => import('./destinos/destinos.module').then( m => m.DestinosPageModule)
+  },  {
+    path: 'galeria',
+    loadChildren: () => import('./galeria/galeria.module').then( m => m.GaleriaPageModule)
   },
+
 
 
 
