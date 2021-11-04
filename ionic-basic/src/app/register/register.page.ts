405669ba48595145ce0029441224ddf416df7fe4
America Gonzaga <america.gonzagatn@uanl.edu.mx>
2021-11-04 12:08:17 -0600

    Practica 14. Loading controller, segment button, tipos de spiner, bugfix login

--- ionic-basic/src/app/register/register.page.ts
+++ ionic-basic/src/app/register/register.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 import { AuthService } from '../service/auth.service';
 import { User } from '../shared/user.class';
 
@@ -10,17 +11,41 @@ import { User } from '../shared/user.class';
 })
 export class RegisterPage implements OnInit {
   user: User = new User();
-  constructor(private autSvc: AuthService, private router: Router) { }
+  constructor(
+    private autSvc: AuthService,
+    private router: Router,
+    public loadingController: LoadingController) { }
 
   ngOnInit() {
   }
 
   async onRegister(){
-    const user = await this.autSvc.onRegister(this.user);
-    if(user!=undefined && user.code == undefined){
-      console.log("Successfully created user!");
-      this.router.navigate(['/']);
-    }
+    this.presentLoadingWithOptions();
+    this.autSvc.onRegister(this.user).then((user: any)=>{
+      if(user!=undefined && user.code == undefined){
+        console.log("Successfully created user!");
+        this.router.navigate(['/']);
+      }
+        this.loadingController.dismiss();
+    }).catch(error=>{
+        this.loadingController.dismiss();
+      console.log(error);
+    });;
+
   }
 
+  async presentLoadingWithOptions() {
+    const loading = await this.loadingController.create({
+      //spinner: null,
+      //duration: 800,
+      message: 'Iniciando sesion...',
+      translucent: true,
+      //cssClass: 'custom-class custom-loading',
+      backdropDismiss: true
+    });
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
+    console.log('Loading dismissed with role:', role);
+  }
 }
