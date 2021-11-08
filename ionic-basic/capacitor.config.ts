645410652988744e0425afd45eef0b1a8d5329bd
David-VR0 <david.97.vargas@outlook.com>
2021-11-08 00:29:34 -0600

    npx cap init

---
+++ ionic-basic/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'como.ionics.starter',
+  appName: 'ionic-basic-1865715',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
