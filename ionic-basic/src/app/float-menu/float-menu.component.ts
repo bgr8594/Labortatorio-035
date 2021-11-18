4e31370f55a8f08837eebaab342e87d7ea6c20d8
LuisOlan <60374196+LuisOlan@users.noreply.github.com>
2021-11-18 17:24:45 -0600

    Practica 6

---
+++ ionic-basic/src/app/float-menu/float-menu.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuElementsInterface } from './menu.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-float-menu',
+  templateUrl: './float-menu.component.html',
+  styleUrls: ['./float-menu.component.scss'],
+})
+export class FloatMenuComponent implements OnInit {
+
+  datosMenu: MenuElementsInterface[] =[
+    {nombre: 'Alumnos',enlace:'/alumnos',
+  icono:'school-outline'},
+    {nombre: 'Receteas',enlace:'/recetas',
+    icono:'restaurant-outline'},
+    {nombre: 'Tabs',enlace:'/tabs',
+    icono:'folder-outline'}
+  ];
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {}
+
+  navegar(link: string){
+    this.router.navigate([link]);
+  }
+
+}
---
+++ ionic-basic/src/app/float-menu/menu.model.ts
@@ -0,0 +1,5 @@
+export interface MenuElementsInterface{
+    nombre: string;
+    enlace: string;
+    icono: string;
+}
